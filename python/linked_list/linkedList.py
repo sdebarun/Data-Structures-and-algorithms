c41db23041bbd07cf5df8f91aa77675ea79dbc74
sdebarun <sdebarun04@gmail.com>
2022-06-24 21:10:20 +0530

    adding the comments and printing

--- python/linked_list/linkedList.py
+++ python/linked_list/linkedList.py
@@ -1,3 +1,10 @@
+
+# Todo : custom linked list implementation in python
+# Todo : append  and prepend  has O(1)
+# Todo : insert will have O(n) - yet to be impleneted
+# Todo : delete will have O(n) - yet to be impleneted
+# Todo : The list will start with one node having value 10
+
 class myLinekdList:
     length = 0
     def __init__(self, value) -> None:
@@ -7,6 +14,7 @@ class myLinekdList:
         }
         self.tail = self.head
         self.length = self.length + 1
+
     def append(self,value) :
         new_node = {
             "value" : value,
@@ -28,12 +36,26 @@ class myLinekdList:
         self.length = self.length + 1
         return self.head
 
+    def printList(self) : 
+        linkedList = []
+        currentNode = self.head
+        while currentNode != None : 
+            # print(currentNode)
+            linkedList.append(currentNode['value'])
+            currentNode = currentNode['next']
+        return linkedList
+    
+    def delete(value) :
+        pass
 
 
 myLinkedList = myLinekdList(10)
-print(myLinkedList.append(12))
-print(myLinkedList.append(13))
+myLinkedList.append(12)
+myLinkedList.append(13)
 # print(myLinekdList.viewList())
-print(myLinkedList.prepend(1))
+myLinkedList.prepend(1)
 
+#! printing out the linked list and its length
+#! printing the linked list in 
+print(myLinkedList.printList())
 print(myLinkedList.length)
